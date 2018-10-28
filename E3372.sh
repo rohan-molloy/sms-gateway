bb382755532988f705311ef871fa58ad4e64f51a
Rohan <10357179+rohan-molloy@users.noreply.github.com>
2018-10-29 04:21:20 +1300

    Update E3372.sh

--- E3372.sh
+++ E3372.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
 
-urlbase="http://192.168.8.1/api"
-curlopts="-fSsL"
+webroot="http://192.168.8.1/api"
+curl_options="-fsSLH Host:hi.link"
 
 #
 # Get a cookie/session id
 #
-RESPONSE=`curl $curlopts -X GET $urlbase/webserver/SesTokInfo`
+RESPONSE=`curl $curl_options -X GET $webroot/webserver/SesTokInfo`
 COOKIE=`echo "$RESPONSE"| grep SessionID=| cut -b 10-147`
 TOKEN=`echo "$RESPONSE"| grep TokInfo| cut -b 10-41`
 
@@ -19,80 +19,80 @@ fi
 
 case "$1" in
 connect) 
-    curl "$curlopts" \
-    -X POST "$urlbase/dialup/dial" 
+    curl $curl_options \
+    -X POST "$webroot/dialup/dial" \
     -H "Cookie:$COOKIE" \
     -H "__RequestVerificationToken:$TOKEN" \
-    -d"<request><Action>1</Action></request>"
+    -d"<request><Action>1</Action></request>" | $pretty_print
 ;;
 disconnect)
-    curl "$curlopts" \ 
-    -X POST "$urlbase/dialup/dial" \
+    curl $curl_options \ 
+    -X POST "$webroot/dialup/dial" \
     -H "Cookie:$COOKIE" \
     -H "__RequestVerificationToken:$TOKEN" \
-    -d "<request><Action>1</Action></request>"
+    -d "<request><Action>1</Action></request>" | $pretty_print
 ;;
 send_sms)
-    curl "$curlopts" \ 
-    -X POST "$urlbase/sms/send-sms" \
+    curl $curl_options \ 
+    -X POST "$webroot/sms/send-sms" \
     -H "Cookie:$COOKIE" \
     -H "__RequestVerificationToken:$TOKEN" \
     -d "<request><Index>-1</Index><Phones><Phone>$2</Phone></Phones><Sca>$sca</Sca><Content>$3</Content><Length>${#3}</Length><Reserved>1</Reserved><Date>-1</Date></request>"
 ;;
 get_sms)
-    curl "$curlopts" \ 
-    -X POST "$urlbase/sms/sms-list" \
+    curl $curl_options \ 
+    -X POST "$webroot/sms/sms-list" \
     -H "Cookie:$COOKIE" \
     -H "__RequestVerificationToken:$TOKEN" \
     -d "<request><PageIndex>1</PageIndex><ReadCount>"${2:-1}"</ReadCount><BoxType>1</BoxType><SortType>0</SortType><Ascending>0</Ascending><UnreadPreferred>1</UnreadPreferred></request>"|$pretty_print
 ;;
 sms_count)
-    curl "$curlopts" \
-    -X GET "$urlbase/sms/sms-count" \
+    curl $curl_options \
+    -X GET "$webroot/sms/sms-count" \
     -H "Cookie:$COOKIE" \
     -H "__RequestVerificationToken:$TOKEN" | $pretty_print
 ;;
 traffic_statistics)
-    curl "$curlopts" \
-    -X GET "$urlbase/monitoring/traffic-statistics" \
+    curl $curl_options \
+    -X GET "$webroot/monitoring/traffic-statistics" \
     -H "Cookie:$COOKIE" \
-    -H "__RequestVerificationToken:$TOKEN" 
+    -H "__RequestVerificationToken:$TOKEN" | $pretty_print
 ;;
 check_notifications)
-    curl "$curlopts" \
-    -X GET "$urlbase/monitoring/check-notifications" \
+    curl $curl_options \
+    -X GET "$webroot/monitoring/check-notifications" \
     -H "Cookie:$COOKIE" \
     -H "__RequestVerificationToken:$TOKEN" | $pretty_print
 ;;
 status)
-    curl "$curlopts" \ 
-    -X GET "$urlbase/monitoring/status" \
+    curl $curl_options \
+    -X GET "$webroot/monitoring/status" \
     -H "Cookie:$COOKIE" \
     -H "__RequestVerificationToken:$TOKEN" | $pretty_print
 ;;
 dialup_connection)
-    curl "$curlopts" \
-    -X GET "$urlbase/dialup/connection" \
+    curl $curl_options \
+    -X GET "$webroot/dialup/connection" \
     -H "Cookie:$COOKIE" \
-    -H "__RequestVerificationToken:$TOKEN" 
+    -H "__RequestVerificationToken:$TOKEN" | $pretty_print
 ;;
 device_information)
-    curl "$curlopts" \
-    -X GET "$urlbase/device/information" \
+    curl $curl_options \
+    -X GET "$webroot/device/information" \
     -H "Cookie:$COOKIE" \
     -H "__RequestVerificationToken:$TOKEN" | $pretty_print
 ;;
 
 send_ussd)
-    curl "$curlopts" \
-    -X POST "$urlbase/ussd/send" \
+    curl $curl_options \
+    -X POST "$webroot/ussd/send" \
     -H "Cookie:$COOKIE" \
     -H "__RequestVerificationToken:$TOKEN" \
-    -d "<Request><Content>$2</Content><CodeType>CodeType</CodeType></Request>";
+    -d "<Request><Content>$2</Content><CodeType>CodeType</CodeType></Request>"| $pretty_print;
 ;;
 get_ussd)
-    curl "$curlopts" \
-    -X POST "$urlbase/ussd/get" \
+    curl $curl_options \
+    -X POST "$webroot/ussd/get" \
     -H "Cookie: $COOKIE" \
     -H "__RequestVerificationToken: $TOKEN" \
     -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
@@ -100,35 +100,49 @@ get_ussd)
 ;;
 *) 
 cat >&2 << EOF
-### HILINK E3372 TOOL (rohan@rmolloy.io)
+#############################################
+#    HILINK E3372 TOOL (rohan@rmolloy.io)   #
+#############################################
 connect) 
     Brings the modem online
+    
 disconnect)
     Brings the modem offline
+    
 send_sms)
     Send an sms message
-    Usage: send_sms mobile_number message
+    Usage: send_sms '\$mobile_number' '\$message'
+    
 get_sms)
     Poll the sms inbox
-    Usage: get_sms count
+    Usage: get_sms '\$count'
+    
 sms_count)
     Query number of SMS message
+    
 traffic_statistics)
     Report traffic statistics 
+    
 check_notifications)
     Poll for new notification 
+    
 status)
     Report device status 
+    
 dialup_connection)
     Report dialup connection 
+    
 device_information)
     Report device information
+    
 send_ussd)
     Send a USSD request 
-    Usage: send_ussd number  
+    Usage: send_ussd '\$ussd_number'  
+    
 get_ussd)
     Query USSD response 
-    Usage: get_ussd number
+    Usage: get_ussd '\$ussd_number'
+
 EOF
 ;;
 esac 
